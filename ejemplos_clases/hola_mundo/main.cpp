03b9678663bf985a22d70bccc72c1a3653d58981
Alberto Valero <alberto.valero@bq.com>
2024-04-15 09:09:51 +0200

    curso cpp

---
+++ ejemplos_clases/hola_mundo/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    string a;
+    cout << "Introduce tu nombre\n";
+    cin >> a;
+    cout << "Hola " << a << "\n";
+    return 0;
+}
