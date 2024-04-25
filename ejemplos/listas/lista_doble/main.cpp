015ba6d2b69c74d1dcb5885ebd20b63c710b499c
Alberto Valero <alberto.valero@bq.com>
2024-04-25 09:27:20 +0200

    lista doble

--- ejemplos/listas/lista_doble/main.cpp
+++ ejemplos/listas/lista_doble/main.cpp
@@ -1,4 +1,3 @@
-
 #include <memory>
 #include <functional>
 #include <iostream>
@@ -8,11 +7,8 @@ using namespace std;
 template <typename T>
 struct Node
 {
-  Node(T const &data,
-       shared_ptr<Node<T>> const &prev = nullptr,
-       shared_ptr<Node<T>> const &next = nullptr) : pData(make_shared<T>(data)), pNext(next), pPrev(prev)
-  {
-  }
+  Node(T const &data, shared_ptr<Node<T>> const &prev = nullptr, shared_ptr<Node<T>> const &next = nullptr)
+      : pData(make_shared<T>(data)), pNext(next), pPrev(prev) {}
   shared_ptr<T> pData;
   shared_ptr<Node<T>> pNext;
   shared_ptr<Node<T>> pPrev;
@@ -39,10 +35,13 @@ void forEachReverse(shared_ptr<Node<T>> pTail, function<void(T const &)> f)
 template <typename T>
 void push_back(shared_ptr<Node<T>> &pHead, T const &data)
 {
-  if (pHead->pNext == nullptr)
+  if (!pHead)
   {
-    pHead->pNext = make_shared<Node<T>>(data);
-    pHead->pNext->pPrev = pHead;
+    pHead = make_shared<Node<T>>(data);
+  }
+  else if (!pHead->pNext)
+  {
+    pHead->pNext = make_shared<Node<T>>(data, pHead);
   }
   else
   {
@@ -51,123 +50,107 @@ void push_back(shared_ptr<Node<T>> &pHead, T const &data)
 }
 
 template <typename T>
-void push_back(shared_ptr<Node<T>> &pHead, shared_ptr<Node<T>> &pNew)
+void push_front(shared_ptr<Node<T>> &pHead, T const &data)
 {
-  if (pHead->pNext == nullptr)
+  auto newNode = make_shared<Node<T>>(data, nullptr, pHead);
+  if (pHead)
   {
-    pHead->pNext = pNew;
-    pNew->pPrev = pHead;
+    pHead->pPrev = newNode;
+  }
+  pHead = newNode;
+}
+
+template <typename T>
+shared_ptr<Node<T>> pop_back(shared_ptr<Node<T>> &pHead)
+{
+  if (!pHead)
+    return nullptr;
+
+  if (!pHead->pNext)
+  {
+    auto retNode = pHead;
+    if (pHead->pPrev)
+    {
+      pHead->pPrev->pNext = nullptr;
+    }
+    pHead = pHead->pPrev;
+    return retNode;
   }
   else
   {
-    push_back(pHead->pNext, pNew);
+    return pop_back(pHead->pNext);
   }
 }
 
 template <typename T>
-void push_front(shared_ptr<Node<T>> &p, T const &data)
+shared_ptr<Node<T>> pop_front(shared_ptr<Node<T>> &pHead)
 {
-  if (p->pPrev == nullptr)
-  {
-    p->pPrev = make_shared<Node<T>>(data);
-    p->pPrev->pNext = p;
-  }
-  else
-  {
-    push_front(p->pPrev, data);
-  }
-}
+  if (!pHead)
+    return nullptr;
 
-template <typename T>
-void push_front(shared_ptr<Node<T>> &p, shared_ptr<Node<T>> &pNew)
-{
-  if (p->pPrev == nullptr)
+  auto retNode = pHead;
+  pHead = pHead->pNext;
+  if (pHead)
   {
-    p->pPrev = pNew;
+    pHead->pPrev = nullptr;
   }
-  else
-  {
-    push_front(p->pPrev, pNew);
-  }
-}
-
-template <typename T>
-shared_ptr<Node<T>> pop_back(shared_ptr<Node<T>> &p)
-{
-  if (p->pNext == nullptr)
-  {
-    p->pPrev->pNext = nullptr;
-    return p;
-  }
-  else
-  {
-    return pop_back(p->pNext);
-  }
-}
-
-template <typename T>
-shared_ptr<Node<T>> pop_front(shared_ptr<Node<T>> &p)
-{
-  if (p->pPrev == nullptr)
-  {
-    p->pNext->pPrev = nullptr;
-    return p;
-  }
-  else
-  {
-    return pop_front(p->pPrev);
-  }
+  return retNode;
 }
 
 template <typename T>
 void insertAfter(shared_ptr<Node<T>> &p, T const &data)
 {
-  shared_ptr<Node<T>> newNode = make_shared<Node<T>>(data, p, p->pNext);
-  p->pNext->pPrev = newNode;
-  p->pNext = newNode;
-}
-
-template <typename T>
-void insertAfter(shared_ptr<Node<T>> &p, shared_ptr<Node<T>> &newNode)
-{
-  newNode->pPrev = p;
-  newNode->pNext = p->pNext;
-  p->pNext->pPrev = newNode;
+  auto newNode = make_shared<Node<T>>(data, p, p->pNext);
+  if (p->pNext)
+    p->pNext->pPrev = newNode;
   p->pNext = newNode;
 }
 
 template <typename T>
 void insertBefore(shared_ptr<Node<T>> &p, T const &data)
 {
-  shared_ptr<Node<T>> newNode = make_shared<Node<T>>(data, p->pPrev, p);
-  p->pPrev->pNext = newNode;
+  auto newNode = make_shared<Node<T>>(data, p->pPrev, p);
+  if (p->pPrev)
+    p->pPrev->pNext = newNode;
   p->pPrev = newNode;
 }
 
 template <typename T>
-void insertBefore(shared_ptr<Node<T>> &p, shared_ptr<Node<T>> &newNode)
+void swapNodes(shared_ptr<Node<T>> &a, shared_ptr<Node<T>> &b)
 {
-  newNode->pPrev = p->pPrev;
-  newNode->pNext = p;
-  p->pPrev->pNext = newNode;
-  p->pPrev = newNode;
+  // Swap the data held by the nodes
+  auto temp = a->pData;
+  a->pData = b->pData;
+  b->pData = temp;
 }
 
 template <typename T>
-shared_ptr<Node<T>> filter(shared_ptr<Node<T>> const &pHead, function<bool(T const &)> f)
+void bubbleSort(shared_ptr<Node<T>> &head, function<bool(shared_ptr<Node<T>>, shared_ptr<Node<T>>)> gt = [](shared_ptr<Node<T>> a, shared_ptr<Node<T>> b)
+                                           { return *(a->pData) > *(b->pData); })
 {
-  shared_ptr<Node<T>> newHead = nullptr;
-  for (auto it = pHead; it != nullptr; it = it->pNext)
+  bool swapped;
+  shared_ptr<Node<T>> p;
+  shared_ptr<Node<T>> lptr = nullptr;
+
+  if (head == nullptr)
+    return;
+
+  do
   {
-    if (f(*(it->pData)))
+    swapped = false;
+    p = head;
+
+    while (p->pNext != lptr)
     {
-      if (newHead == nullptr)
-        newHead = it;
-      else
-        push(newHead, it);
+      if (gt(p, p->pNext))
+      {
+        swapNodes(p, p->pNext);
+        swapped = true;
+      }
+      p = p->pNext;
     }
-  }
-  return newHead;
+    lptr = p;
+  } while (swapped);
 }
 
 struct Person
@@ -178,15 +161,15 @@ struct Person
 
 int main()
 {
-
   shared_ptr<Node<Person>> pHead = make_shared<Node<Person>>(Person{"John", 30});
-  push_back<Person>(pHead, Person{"Maria", 30});
-  push_back<Person>(pHead, Person{"Luis", 21});
+  push_back(pHead, Person{"Maria", 35});
+  push_back(pHead, Person{"Luis", 21});
 
-  shared_ptr<Node<Person>> other = make_shared<Node<Person>>(Person{"Lucia", 30});
+  forEach<Person>(pHead, [](Person const &p)
+                  { cout << p.name << "-" << p.age << endl; });
 
-  push_back<Person>(pHead, other);
-  insertAfter<Person>(pHead->pNext, Person{"Pedro", 30});
+  bubbleSort<Person>(pHead, [](shared_ptr<Node<Person>> a, shared_ptr<Node<Person>> b)
+                     { return a->pData->age > b->pData->age; });
 
   forEach<Person>(pHead, [](Person const &p)
                   { cout << p.name << "-" << p.age << endl; });
